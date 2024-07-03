115fe1d018ea2e74a644022ba15e451e81554d07
omkarkhambekar <khambekaromkar123@gmail.com>
2024-07-03 14:32:38 +0530

    E-Comm-Proj

---
+++ src/main/java/com/omkar/E_Comm_Proj/model/Product.java
@@ -0,0 +1,36 @@
+package com.omkar.E_Comm_Proj.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private String category;
+    private BigDecimal price;
+
+
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+
+    @Lob
+    private byte[] imageData;
+
+}
---
+++ src/main/java/com/omkar/E_Comm_Proj/service/ProductService.java
@@ -0,0 +1,47 @@
+package com.omkar.E_Comm_Proj.service;
+
+import com.omkar.E_Comm_Proj.model.Product;
+import com.omkar.E_Comm_Proj.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageData(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(int id) {
+
+        productRepository.deleteById(id);
+    }
+}
